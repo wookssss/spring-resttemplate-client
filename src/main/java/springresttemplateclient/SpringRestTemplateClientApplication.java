3cce4bc205aa3196de23f23370fe0e56942146f5
Jeongwook Woo <dnwjddnr115@naver.com>
2025-01-31 13:14:36 +0900

    init commit

---
+++ src/main/java/springresttemplateclient/SpringRestTemplateClientApplication.java
@@ -0,0 +1,11 @@
+package springresttemplateclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestTemplateClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestTemplateClientApplication.class,args);
+    }
+}
